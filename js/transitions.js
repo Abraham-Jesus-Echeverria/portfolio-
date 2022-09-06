e56a5b605899eecedc8a309144dab3c7fd98b7a2
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2022-09-05 23:16:58 -0500

    transiciones de los elementos del header

--- js/transitions.js
+++ js/transitions.js
@@ -1,14 +1,24 @@
 (()=>{ 
-    let $imgLogo = document.querySelector(".header__img"); 
-    let $textHeader = document.querySelectorAll(".header_text"); 
-    let arrayHeaderText = [...$textHeader]; 
-    console.log(arrayHeaderText[0]);     
+    let $headerText = document.querySelectorAll(".header_text");  
+    let arrayHeaderText = [...$headerText]; 
+    let $rs_container = document.querySelector(".card_rs");  
+    let $container_image = document.querySelector(".container_image"); 
+    let $nav_ul = document.querySelector(".nav_ul"); 
+    
 
-    document.addEventListener("DOMContentLoaded", ()=>{   
-        setTimeout(()=>{ 
-            arrayHeaderText[0].classList.add("header_text_transition");  
-            arrayHeaderText[1].classList.add("header_text_transition"); 
-        },1500)
-        }); 
+    document.addEventListener("DOMContentLoaded", ()=>{ 
+        console.log("se esta disparando el evento"); 
+    setTimeout(()=>{ 
+        arrayHeaderText[0].classList.add("header_text_transition");  
+        arrayHeaderText[1].classList.add("header_text_transition"); 
+    },500);  
+    setTimeout(()=>{ 
+        $rs_container.classList.add("card_rs_transition");
+        $container_image.classList.add("container_image_transition"); 
+        $nav_ul.classList.add("nav_ul_transition"); 
+    },600); 
+     
+
+}); 
 })(); 
 
