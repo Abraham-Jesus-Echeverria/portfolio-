9b389a9f44bd07386b9aab69e52640ec5e3856bf
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2022-09-07 19:33:59 -0500

    reparacion tiempos de transiciones

--- js/transitions.js
+++ js/transitions.js
@@ -6,17 +6,17 @@
     let $nav_ul = document.querySelector(".nav_ul"); 
     
 
-    document.addEventListener("DOMContentLoaded", ()=>{ 
+    window.addEventListener("load", ()=>{ 
         console.log("se esta disparando el evento"); 
-    setTimeout(()=>{ 
+    
         arrayHeaderText[0].classList.add("header_text_transition");  
-        arrayHeaderText[1].classList.add("header_text_transition"); 
-    },500);  
-    setTimeout(()=>{ 
-        $rs_container.classList.add("card_rs_transition");
-        $container_image.classList.add("container_image_transition"); 
-        $nav_ul.classList.add("nav_ul_transition"); 
-    },600); 
+        arrayHeaderText[1].classList.add("header_text_transition");  
+
+        setTimeout(()=>{ 
+            $rs_container.classList.add("card_rs_transition");
+            $container_image.classList.add("container_image_transition"); 
+            $nav_ul.classList.add("nav_ul_transition"); 
+        },800);  
      
 
 }); 
